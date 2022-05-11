caed77afa83576db149a64de1c6de030aeda3101
Alfrugo <adiez@mtm-inc.net>
2022-05-10 20:59:06 -0500

    added logo navbar

--- home/src/App.js
+++ home/src/App.js
@@ -1,9 +1,10 @@
 
+import Navbar from './components/Navbar'
+
 function App() {
   return (
     <div>
-          <h1 className="text-2xl font-bold underline">
-hello world</h1>
+        <Navbar />
     </div>
   );
 }
--- home/src/components/Navbar.js
+++ home/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Logo from '../assets/Alfredo-Diez.svg'
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-800 text-cyan-600'>
+            <img src={Logo} alt='Alfredo Diez' style={{width: '150px'}} />
         </div>
     )
 }
